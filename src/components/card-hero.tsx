b0dc1c0c8297f02723b1d8dc36e19ebd4bd6509d
wwwacedo <eng.marianammacedo@gmail.com>
2024-11-14 21:44:47 -0300

    inserindo gráfico

--- src/components/card-hero.tsx
+++ src/components/card-hero.tsx
@@ -1,54 +1,52 @@
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 import { Leaf, Car, Zap, Beef } from "lucide-react";
 import Image from "next/image";
+import { PieChartSection } from "./pie-chart";
 
 export default function CardHero({ nome }: { nome: string }) {
   return (
-    <Card className="w-full max-w-sm bg-white border-4 border-green-500 rounded-xl overflow-hidden shadow-xl">
+    <Card className="w-full max-w-sm bg-white  bg-gradient-to-br from-green-500 to-green-600 rounded-xl overflow-hidden shadow-xl">
       <div className="py-2 px-4">
         <div className="flex gap-2 items-center">
-          <div className="relative h-[50px] w-[50px] rounded-full">
+          <div className="relative flex items-center justify-center h-[50px] w-[50px] rounded-full bg-white">
             <Image
               src="/carbon-neutral.png"
               alt="Carbon Neutral"
-              width={50}
-              height={50}
+              width={40}
+              height={40}
             />
           </div>
           <div className="h-[50px]">
-            <h4 className="text-sm">Carbon Heroes</h4>
-            <h1 className="text-xl font-bold">{nome}</h1>
+            <h4 className="text-sm text-foreground/80">Carbon Heroes</h4>
+            <h1 className="text-xl font-bold text-white">{nome}</h1>
           </div>
         </div>
       </div>
 
-      <CardContent className="p-4 h-full flex flex-col bg-gray-200">
+      <CardContent className="p-4 h-full flex gap-4 flex-col bg-gray-200">
         
-				{/* GRÁFICO */}
-        <div className="flex-grow relative mb-2">
-
-				</div>
+				<PieChartSection/>
 
         <div className="space-y-2 text-sm">
-          <div className="flex justify-between items-center bg-gray-100 p-2 rounded">
+          <div className="flex justify-between items-center bg-gray-50 p-2 rounded">
             <span className="font-semibold flex items-center">
               <Leaf className="w-5 h-5 mr-2 text-green-500" /> Pegada de Carbono
             </span>
             <span className="font-bold">2.5 tons/year</span>
           </div>
-          <div className="flex justify-between items-center bg-gray-100 p-2 rounded">
+          <div className="flex justify-between items-center bg-gray-50 p-2 rounded">
             <span className="font-semibold flex items-center">
               <Car className="w-5 h-5 mr-2 text-blue-500" /> Transporte
             </span>
             <span className="font-bold">100 L/day</span>
           </div>
-          <div className="flex justify-between items-center bg-gray-100 p-2 rounded">
+          <div className="flex justify-between items-center bg-gray-50 p-2 rounded">
             <span className="font-semibold flex items-center">
               <Zap className="w-5 h-5 mr-2 text-yellow-500" /> Energia em Casa
             </span>
             <span className="font-bold">90%</span>
           </div>
-          <div className="flex justify-between items-center bg-gray-100 p-2 rounded">
+          <div className="flex justify-between items-center bg-gray-50 p-2 rounded">
             <span className="font-semibold flex items-center">
               <Beef className="w-5 h-5 mr-2 text-red-500" /> Alimentação
             </span>
