d9f9def89b7a9eb458ff6fb12d6d26815a8fb288
wwwacedo <eng.marianammacedo@gmail.com>
2024-11-17 10:56:49 -0300

    ok

--- src/lib/calc.ts
+++ src/lib/calc.ts
@@ -4,7 +4,7 @@ const SEMANAS_ANO = 52;
 const MESES_ANO = 12;
 
 export function kgParaTons(kg: number) {
-  return Number((kg / 1000).toFixed(2));
+  return Number((kg / 1000).toFixed(4));
 }
 
 export function calcularTransporteIndividual(
