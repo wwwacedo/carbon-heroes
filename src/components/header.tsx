22921dfa97954364b2126a12c02ec25a9e5ee470
wwwacedo <eng.marianammacedo@gmail.com>
2024-11-16 21:45:41 -0300

    hamburguinho

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,15 @@
-import { Leaf } from "lucide-react";
-import Link from "next/link";
-import { orbitron } from "@/app/fonts/fonts";
+'use client'
+
+import { useState } from "react"
+import { Leaf, Menu, X } from 'lucide-react'
+import Link from "next/link"
+import { orbitron } from "@/app/fonts/fonts"
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen)
+
   return (
     <header className="m-auto bg-gray-50 border-b">
       <div className="container m-auto px-4 py-2 flex items-center">
@@ -12,27 +19,57 @@ export default function Header() {
           </div>
           <h2 className={`${orbitron.className}`}>Carbon Heroes</h2>
         </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
+        <div className="ml-auto flex items-center">
+          <nav className="hidden sm:flex gap-6">
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="#sobre"
+            >
+              Sobre
+            </Link>
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="#recursos"
+            >
+              Recursos
+            </Link>
+            <Link
+              className="text-sm font-medium hover:underline underline-offset-4"
+              href="#como-jogar"
+            >
+              Como jogar
+            </Link>
+          </nav>
+          <button onClick={toggleMenu} className="sm:hidden">
+            {isMenuOpen ? <X className="h-6 w-6" /> : <Menu className="h-6 w-6" />}
+          </button>
+        </div>
+      </div>
+      {isMenuOpen && (
+        <nav className="sm:hidden container m-auto px-4 py-2 flex flex-col gap-2">
           <Link
             className="text-sm font-medium hover:underline underline-offset-4"
             href="#sobre"
+            onClick={toggleMenu}
           >
             Sobre
           </Link>
           <Link
             className="text-sm font-medium hover:underline underline-offset-4"
             href="#recursos"
+            onClick={toggleMenu}
           >
             Recursos
           </Link>
           <Link
             className="text-sm font-medium hover:underline underline-offset-4"
             href="#como-jogar"
+            onClick={toggleMenu}
           >
             Como jogar
           </Link>
         </nav>
-      </div>
+      )}
     </header>
-  );
+  )
 }
